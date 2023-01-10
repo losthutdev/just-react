889ce009db73f07dd9e08ece313ec580d596b8a0
Andrea <losthut@gmail.com>
2023-01-10 16:57:30 +0800

    full commit

---
+++ src/EnrolmentForm.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function EnrolmentForm(props) {
+
+    const [firstName, setFirstName] = useState("");
+
+    const [lastName, setLastName] = useState("");
+
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+
+    const handleSubmit = (event) => {
+
+        setWelcomeMessage(`Welcome ${firstName} ${lastName}`);
+        props. setUpdatedSeats(props.currentSeats-1);
+        event.preventDefault();
+
+    };
+
+    return (
+
+        <div>
+
+            <form className="enrolForm" onSubmit={handleSubmit}>
+
+                <h1>{props.chosenProgram} Student Details</h1>
+
+                <label>First name:</label>
+
+                <input
+
+                    type="text"
+
+                    name="fname"
+
+                    onBlur={(event) => setFirstName(event.target.value)}
+
+                />
+
+                <br />
+
+                <label>Last name:</label>
+
+                <input
+
+                    type="text"
+
+                    name="lname"
+
+                    onBlur={(event) => setLastName(event.target.value)}
+
+                />
+
+                <br />
+
+                <br />
+
+                <input type="submit" value="Submit" />
+
+                <br />
+
+                <label id="studentMsg" className="message">
+
+                    {welcomeMessage}
+
+                </label>
+
+            </form>
+
+        </div>
+
+    );
+
+}
+
+export default EnrolmentForm;
